0a39b61e0bdd6b0e56af9688c06694c194f55440
Harlei Melo <idzeronee@gmail.com>
2024-03-19 20:01:02 -0300

    Adicionando campos login e senha

--- atividade_roussian/src/main/java/com/domain/usuario.java
+++ atividade_roussian/src/main/java/com/domain/usuario.java
@@ -1,20 +1,19 @@
 package com.domain;
 
 public class usuario {
-    private String nome;
-    private Long id;
-
+    private String login;
+    private String senha;
     
-    public String getNome() {
-        return nome;
+    public String getLogin() {
+        return login;
     }
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setLogin(String login) {
+        this.login = login;
     }
-    public Long getId() {
-        return id;
+    public String getSenha() {
+        return senha;
     }
-    public void setId(Long id) {
-        this.id = id;
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 }
