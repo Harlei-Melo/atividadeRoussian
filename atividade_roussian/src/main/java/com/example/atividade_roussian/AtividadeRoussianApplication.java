4e7e691bdf71786b244ba96533f77ec954eb1489
Harlei Melo <idzeronee@gmail.com>
2024-03-19 19:50:31 -0300

    Criando projeto Spring Boot

---
+++ atividade_roussian/src/main/java/com/example/atividade_roussian/AtividadeRoussianApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividade_roussian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeRoussianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeRoussianApplication.class, args);
+	}
+
+}
---
+++ atividade_roussian/src/test/java/com/example/atividade_roussian/AtividadeRoussianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividade_roussian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeRoussianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
