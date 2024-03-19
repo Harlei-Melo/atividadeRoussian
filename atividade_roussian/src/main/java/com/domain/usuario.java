a2ba399a839df08d1c977104641898db6903952a
Harlei Melo <idzeronee@gmail.com>
2024-03-19 19:59:11 -0300

    Criando classe Usuario

---
+++ atividade_roussian/src/main/java/com/domain/usuario.java
@@ -0,0 +1,20 @@
+package com.domain;
+
+public class usuario {
+    private String nome;
+    private Long id;
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
